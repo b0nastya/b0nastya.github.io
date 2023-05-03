f156be673ba551d6089b09d8b2ccac74af395380
b0nastya <132206414+b0nastya@users.noreply.github.com>
2023-05-03 14:16:07 +0400

    Update update_value.php

--- update_value.php
+++ update_value.php
@@ -1,6 +1,6 @@
 <?php
 $new_value = $_POST['value'];
-$file_path = '/value.txt'; // указываем путь к файлу с текущим значением
+$file_path = 'value.txt'; // указываем путь к файлу с текущим значением
 file_put_contents($file_path, $new_value); // записываем новое значение в файл
 echo $new_value; // возвращаем новое значение для отображения на странице
 ?>
